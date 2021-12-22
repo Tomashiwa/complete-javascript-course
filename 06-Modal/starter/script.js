4fd996bdb6071d20471ab0adedf078c2883103cd
Tomashiwa <wongqinliang@gmail.com>
2021-12-22 15:54:34 +0800

    Project 2 and 3

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,1 +1,28 @@
 'use strict';
+
+const btnsShowModal = document.querySelectorAll('.show-modal');
+const modal = document.querySelector('.modal');
+const btnCloseModal = document.querySelector('.close-modal');
+const overlay = document.querySelector('.overlay');
+
+const openModal = function () {
+  // Removing the class "hidden" from the modal to make it visible
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+const closeModal = function () {
+  // Adding the class "hidden" from the modal to make it invisible
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+btnsShowModal.forEach(btn => btn.addEventListener('click', openModal));
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+  // Escape button pressed and the modal is visible
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -1,1 +1,96 @@
 'use strict';
+
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
+const curr0El = document.querySelector('#current--0');
+const curr1El = document.querySelector('#current--1');
+const score0El = document.querySelector('#score--0');
+const score1El = document.querySelector('#score--1');
+const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+
+let scores, currScore, turn, isPlaying;
+
+const initialise = function () {
+  player0El.classList.remove('player--winner');
+  player1El.classList.remove('player--winner');
+  player0El.classList.add('player--active');
+  player1El.classList.remove('player--active');
+  diceEl.classList.add('hidden');
+
+  curr0El.textContent = 0;
+  curr1El.textContent = 0;
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+
+  scores = [0, 0];
+  currScore = 0;
+  isPlaying = true;
+  turn = 0;
+};
+
+// Switch player
+const switchPlayer = function () {
+  currScore = 0;
+  document.querySelector(`#current--${turn}`).textContent = 0;
+  turn = turn === 0 ? 1 : 0;
+
+  // Add class if not in class list, Remove class if its in class list
+  player0El.classList.toggle('player--active');
+  player1El.classList.toggle('player--active');
+};
+
+// Rolling dice
+const rollDice = function () {
+  if (!isPlaying) {
+    return;
+  }
+
+  // Generate random dice roll
+  const value = Math.trunc(Math.random() * 6) + 1;
+  console.log(`Player ${turn} rolled ${value}`);
+
+  // Display dice
+  diceEl.classList.remove('hidden');
+  diceEl.src = `dice-${value}.png`;
+
+  if (value === 1) {
+    switchPlayer();
+  } else {
+    // Add dice to current score
+    currScore += value;
+    document.querySelector(`#current--${turn}`).textContent = currScore;
+  }
+};
+btnRoll.addEventListener('click', rollDice);
+
+// Hold
+const hold = function () {
+  if (!isPlaying) {
+    return;
+  }
+
+  // Update score display
+  scores[turn] += currScore;
+  document.querySelector(`#score--${turn}`).textContent = scores[turn];
+
+  // Determine winner or switch player
+  if (scores[turn] >= 20) {
+    isPlaying = false;
+    // Adding and removing classes to change style and visibility of elements
+    document.querySelector(`.player--${turn}`).classList.add('player--winner');
+    document
+      .querySelector(`.player--${turn}`)
+      .classList.remove('player--active');
+    diceEl.classList.add('hidden');
+  } else {
+    switchPlayer();
+  }
+};
+btnHold.addEventListener('click', hold);
+
+btnNew.addEventListener('click', initialise);
+
+initialise();
