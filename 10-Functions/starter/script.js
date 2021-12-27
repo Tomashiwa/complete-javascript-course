8e6b45be8e15547a9f749788749d91038a20f6d8
Tomashiwa <wongqinliang@gmail.com>
2021-12-27 15:19:48 +0800

    Complete 10, 11-1 & 2

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,1 +1,234 @@
 'use strict';
+
+// Default parameter in function
+/*
+function attack(dmg = 1, type = 'raw') {
+  console.log(`Dealt ${dmg} ${type} damage`);
+}
+attack(10, 'ice');
+attack();
+attack(10);
+// Can put undefined to use the default parameter and continue with later parameters
+attack(undefined, 'shadow');
+*/
+
+// Passing arguments
+/*
+const dmg = 5;
+const player = {
+  health: 10,
+};
+// Primitive are passed as a copy of the value
+// Objects are passed as a reference (memory address)
+function attack(dmg, target) {
+  dmg = 1;
+  target.health -= dmg;
+  console.log(`---Attack with ${dmg} damage---`);
+  console.log(`Target's health:`, target);
+  console.log('------------------------');
+}
+attack(dmg, player);
+console.log(dmg, player);
+*/
+
+// Higher-order functions
+// Functions that take functions as parameters or return a function
+/*
+const oneWord = function (str) {
+  return str.replace(/ /g, '').toLowerCase();
+};
+
+const upperFirstWord = function (str) {
+  const [first, ...other] = str.split(' ');
+  return [first.toUpperCase(), ...other].join(' ');
+};
+
+// A higher-order function that takes in functions as parameers
+const transform = function (str, ...transformers) {
+  let result = str;
+  for (const transformer of transformers) {
+    result = transformer(result);
+  }
+  return result;
+};
+
+console.log(
+  transform('Destroyed Vintage Watch - Part 2', oneWord, upperFirstWord)
+);
+
+// A higher-order function that returns a function
+const createStrega = function (series) {
+  return function (designer) {
+    console.log(`${series} Stregas - ${designer}`);
+  };
+};
+const designStrega = createStrega('Silver Trail');
+designStrega('Estelle Bright');
+
+// Arrow function version
+const createStrega2 = series => designer =>
+  console.log(`${series} Stregas - ${designer}`);
+const designStrega2 = createStrega2('Silver Trail');
+designStrega2('Estelle Bright');
+*/
+
+// "Call" and "Apply" methods in a function
+// Calls the function with a "this" value and arguments given explicity
+// For "Apply", an array is used to pass arguments instead
+/*
+const player = {
+  name: 'Estelle',
+  health: 10,
+  attack: 4,
+};
+const monster = {
+  name: 'Slime',
+  health: 5,
+  attack: 2,
+};
+function attack(target) {
+  // "this" here is undefined as its just a simple function call, no caller
+  target.health -= this.attack;
+  console.log(
+    `${this.name} attacked ${target.name} with ${this.attack} damage, ${target.name}'s health: ${target.health}`
+  );
+}
+// Executing the method "call" on the function "attack" allows us to bind a value to "this"
+attack.call(player, monster);
+// Similar to the method "call", but "apply" takes in array of arguments instead
+attack.apply(player, [monster]);
+*/
+
+// "Bind" method in a function
+// Creates a new function from a function, with a preset "this" and parameters if any is provided
+/*
+const playerAttack = attack.bind(player);
+playerAttack(monster);
+const monsterAttackPlayer = attack.bind(monster, player);
+monsterAttackPlayer();
+monsterAttackPlayer();
+// Practical example - Explicitly set "this" for a callback function of a button
+const buyItem = function () {
+  console.log(this);
+  console.log(`${this.name} have bought a potion...`);
+};
+document.querySelector('.buy').addEventListener('click', buyItem); // "this" is undefined
+document.querySelector('.buy').addEventListener('click', buyItem.bind(player)); // "this" is player
+// Practical example - Partial application (Apply some parameters of a function)
+const formSentence = function (...words) {
+  return words.join(' ');
+};
+const formGreetings = formSentence.bind(null, 'Good', 'Morning');
+console.log(formGreetings('Ms', 'Smith'));
+
+// Replicating "bind" with a higher-order function
+const formPartial = function (...greetings) {
+  return function (...words) {
+    return formSentence(...greetings, ...words);
+  };
+};
+// A function with partial application
+const formGreetings2 = formPartial('Good', 'Morning');
+console.log(formGreetings2('Ms', 'Lloyd'));
+*/
+
+////////////////////////////////////////////////////////////////
+// Coding Challenge #1
+/*
+const poll = {
+  question: 'What is your favourite programming language?',
+  options: ['0: JavaScript', '1: Python', '2: Rust', '3: C++'],
+  answers: new Array(4).fill(0),
+
+  // 1
+  registerNewAnswer() {
+    const question = `${this.question}\n${this.options.join(
+      '\n'
+    )}\n(Write option number)`;
+    const chosen = Number(prompt(question));
+
+    // Increment answers array with Short-ciruiting
+    typeof chosen === 'number' &&
+      chosen >= 0 &&
+      chosen < this.options.length &&
+      this.answers[chosen]++;
+
+    // 4
+    this.displayResults();
+    this.displayResults('string');
+  },
+
+  // 3
+  displayResults(type = 'array') {
+    if (type === 'string') {
+      console.log(`Poll results are ${this.answers.join(', ')}`);
+    } else if (type === 'array') {
+      console.log(this.answers);
+    } else {
+      console.log('Invalid type');
+    }
+  },
+};
+
+// 2
+document
+  .querySelector('.poll')
+  .addEventListener('click', poll.registerNewAnswer.bind(poll));
+
+// 5
+var answers = [5, 2, 3];
+// var answers = [1, 5, 3, 9, 6, 1];
+const displayResultsEx = poll.displayResults.bind(window);
+displayResultsEx();
+*/
+////////////////////////////////////////////////////////////////
+
+// Closure
+// The mechanism whereby a function always has access to the variable environment of the execution context
+// where the function is created, even after the context is gone
+/*
+// Timer with closure
+const scheduleMsg = function (msg) {
+  setTimeout(function () {
+    // Always have access to scheduleMsg's variable environment
+    console.log(msg);
+  }, 2000);
+};
+
+const msg = 'Joshua';
+// Variable in closure takes priority over the scope chain's
+scheduleMsg('Estelle');
+*/
+
+// Immediately invoked function expression (IIFE)
+// A 1-time disposable function that runs once immediately and lost after that
+/*
+// Parenthesis around the functions are necessary to let JS treat it as a function
+// expression
+(function () {
+  console.log('Temporary function');
+})();
+(() => {
+  console.log('Temporary arrow');
+})();
+*/
+
+////////////////////////////////////////////////////////////////
+// Coding Challenge #2
+/*
+(function () {
+  const header = document.querySelector('h1');
+  header.style.color = 'red';
+  document.querySelector('body').addEventListener('click', function () {
+    header.style.color = 'blue';
+  });
+})();
+*/
+
+/*
+The callback function is declared and attached to the body element while having access to the 
+variable environment of its parent function's execution context through closure.
+
+This variable environment provides the header that is needed by the callback function to 
+change the text color to blue.
+*/
--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -1,6 +1,4 @@
 'use strict';
-
-/////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // BANKIST APP
 
@@ -61,16 +59,230 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
-/////////////////////////////////////////////////
+const displayMovements = function (movements) {
+  // Empty out the elements in the list
+  containerMovements.innerHTML = '';
+
+  movements.forEach((movement, index) => {
+    const type = movement > 0 ? 'deposit' : 'withdrawal';
+    const html = `
+    <div class="movements__row">
+      <div class="movements__type movements__type--${type}">${index} ${type}</div>
+      <div class="movements__value">${movement}</div>
+    </div>
+    `;
+
+    // Add html into an element
+    /*
+      eg.
+      <h1>
+        ...
+        NEW_HTML
+      </h1>
+    */
+    containerMovements.insertAdjacentHTML('afterbegin', html);
+  });
+};
+displayMovements(account1.movements);
+
+const calculateDisplayBalance = function (movements) {
+  const balance = movements.reduce((sum, movement) => sum + movement, 0);
+  labelBalance.textContent = `$${balance} EUR`;
+};
+calculateDisplayBalance(account1.movements);
+
+const createUsernames = function (accounts) {
+  accounts.forEach(function (account) {
+    account.username = account.owner
+      .toLowerCase()
+      .split(' ') // Split name into words
+      .map(word => word[0]) // Just take the 1st letter of each word
+      .join('');
+  });
+};
+createUsernames(accounts);
+
 /////////////////////////////////////////////////
 // LECTURES
+// const currencies = new Map([
+//   ['USD', 'United States dollar'],
+//   ['EUR', 'Euro'],
+//   ['GBP', 'Pound sterling'],
+// ]);
+// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
+/////////////////////////////////////////////////
+// Array
+/*
+// Array are objects with access to built in methods
+let arr = [...'estelle'];
+
+// Array.slice(...)
+// Array's .slice behave the same as String's .slice
+console.log(arr.slice(2, 4));
+// Creating shallow copy
+console.log(arr.slice());
+console.log([...arr]);
+
+// Array.splice(...)
+// Delete/Replace/Insert elements starting from an index
+// Note. Mutates the actual array
+// Delete
+arr.splice(2, 3);
+console.log(arr);
+
+// Replace
+arr.splice(2, 3, ...'abc');
+console.log(arr);
+
+// Insert
+arr.splice(0, 0, ...'abc');
+console.log(arr);
+
+// Array.reverse()
+// Reverse the order of items
+// Note. Mutates the actual array
+arr.reverse();
+console.log(arr);
+
+// Array.concat(...)
+// Returns an array that is a concat of 2 arrays
+arr = [...'estelle'];
+console.log(arr.concat([...'bright']));
+
+// Array.join()
+// Join the array elements into a string with a given seperator
+console.log(arr.join('|'));
+
+// Array.at(...)
+// Access an element through an index
+// Similar to [] notation but can use negative index to count from right to left
+arr = [...'estlle'];
+console.log(arr.at(-1)); // last element - e
+*/
+
+// String.at(...)
+
+// ForEach
+/*
+// ForEach with array
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+// For loop has access to index and element via .entries()
+for (const [index, movement] of movements.entries()) {
+  console.log(`[Movement ${index}]: ${movement}`);
+  if (index >= 2) break;
+}
+console.log('-------------------');
+// forEach has access to index, element and the array
+// forEach cannot be "break" or "continue"
+movements.forEach((movement, index, arr) => {
+  console.log(`[Movement ${index}]: ${movement}`);
+  index === arr.length - 1 && console.log(arr);
+});
+
+// ForEach with map
 const currencies = new Map([
   ['USD', 'United States dollar'],
   ['EUR', 'Euro'],
   ['GBP', 'Pound sterling'],
 ]);
+currencies.forEach((value, key, map) => {
+  console.log(`${key}: ${value}`);
+});
 
+// ForEach with set
+const items = new Set(['potion', 'quartz', 'potion', 'fries']);
+// Second parameter is the same as value but was maintained to be consistent with other data structures
+items.forEach((value, _, set) => {
+  console.log(`${value}: ${value}`);
+});
+*/
+
+///////////////////////////////////////////////////////////////////////////////////
+
+// Coding Exercise #1
+/*
+const julia1 = [3, 5, 2, 12, 7];
+const kate1 = [4, 1, 15, 8, 3];
+
+const julia2 = [9, 16, 6, 8, 3];
+const kate2 = [10, 5, 6, 1, 4];
+
+function checkDogs(dogsJulia, dogsKate) {
+  const dogsCorrected = [...dogsJulia];
+  dogsCorrected.splice(0, 1);
+  dogsCorrected.splice(-2, 2);
+  const dogsTotal = [...dogsCorrected, ...dogsKate];
+
+  console.log(dogsTotal);
+
+  dogsTotal.forEach((dog, index) => {
+    const str =
+      dog >= 3 ? `is an adult, and is ${dog} years old` : `is still a puppy 🐶`;
+    console.log(`Dog number ${index + 1} ${str}`);
+  });
+}
+
+checkDogs(julia1, kate1);
+console.log('-------------');
+checkDogs(julia2, kate2);
+*/
+
+///////////////////////////////////////////////////////////////////////////////////
+
+/*
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
-/////////////////////////////////////////////////
+// Array.map(...)
+// Returns a new array containing the results of applying a function on all original array elements
+const eurToUsd = 1.1;
+const movementsUsd = movements.map(movement => movement * eurToUsd);
+// Similar to forEach, has access to the element, index, and the original array
+const movementsLogs = movements.map(
+  (movement, index, arr) =>
+    `Movement ${index + 1}: You've ${
+      movement > 0 ? 'deposited' : 'withdrawed'
+    } $${Math.abs(movement)}.`
+);
+console.log(movements, movementsUsd, movementsLogs);
+
+// Array.filter(...)
+// Returns a new array containing the elements that passed a specificed test function
+// Similar to forEach, has access to the element, index and the original array
+const deposits = movements.filter((movement, index, arr) => movement > 0);
+console.log(deposits);
+const withdrawals = movements.filter(movement => movement < 0);
+console.log(withdrawals);
+
+// Array.reduce(...)
+// Reduces all array elements down to 1 value through an callback and an initial vlaue
+const balance = movements.reduce((sum, movement, index, arr) => {
+  console.log(`iter ${index}: ${sum} before adding ${movement}`);
+  return sum + movement;
+}, 0); // Initial value of accumulator
+console.log(`Total balance: ${balance}`);
+// Example - Find max value
+const max = movements.reduce(
+  (max, curr) => (curr > max ? curr : max),
+  movements[0]
+);
+console.log(movements, max);
+*/
+
+///////////////////////////////////////////////////////////////////////////////////
+
+// Coding Challenge #2
+/*
+const data1 = [5, 2, 4, 1, 15, 8, 3];
+const data2 = [16, 6, 10, 5, 6, 1, 4];
+
+function calcAverageHumanAge(ages) {
+  const humanAges = ages
+    .map(age => (age <= 2 ? 2 * age : 16 + age * 4))
+    .filter(age => age >= 18);
+  console.log(humanAges);
+  return humanAges.reduce((sum, curr) => sum + curr, 0) / humanAges.length;
+}
+console.log(`Results 1: ${calcAverageHumanAge(data1)}`);
+console.log(`Results 2: ${calcAverageHumanAge(data2)}`);
+*/
